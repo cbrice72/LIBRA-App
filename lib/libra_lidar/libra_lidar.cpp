ed49dccb91e7b9fd0b24dab0ed9344f7cbe5ad99
christian-brice <brice.c.aa@m.titech.ac.jp>
2024-03-06 14:08:35 +0900

    added skeleton code for hokuyo lidar

---
+++ lib/libra_lidar/libra_lidar.cpp
@@ -0,0 +1,43 @@
+/******************************************************************************
+ * @file   libra_lidar.cpp
+ * @brief  Control code for LIBRA manipulator Hokuyo LIDAR; implementation file.
+ *
+ * @author Christian Brice
+ * @date   2024/3/6
+ ******************************************************************************/
+
+// Related Header
+#include "libra_lidar.h"
+
+// C++ Standard Library Headers
+//   (none)
+// Other Libraries' Headers
+//   Hokuyo URG
+// TODO...
+// Project Headers
+//   (none)
+
+/* --- TABLE OF CONTENTS ---
+ * !Helper Functions
+ * !Sensor Commands
+ * !Getters & Setters
+ */
+
+/**
+ * @brief Standard constructor.
+ */
+LibraLidar::LibraLidar() {
+    // TODO
+}
+
+//------------------------------------------------------------------------------
+// !Helper Functions
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// !Sensor Commands
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// !Getters & Setters
+//------------------------------------------------------------------------------
---
+++ lib/libra_lidar/libra_lidar.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+ * @file   libra_lidar.h
+ * @brief  Control code for LIBRA manipulator Hokuyo LIDAR; header file.
+ *
+ * @author Christian Brice
+ * @date   2024/3/6
+ ******************************************************************************/
+
+#pragma once
+
+// C++ Standard Library Headers
+//   (none)
+// Other Libraries' Headers
+//   Hokuyo URG
+// TODO...
+// Project Headers
+//   (none)
+
+class LibraLidar {
+  public:
+    LibraLidar();
+
+    // --- Sensor Commands ---
+
+    // --- Getters & Setters ---
+
+  private:
+    // --- Helper Functions ---
+
+    // --- Data Members ---
+};
