d20e7abf236a334867715b5318edd209fa81344c
Christian Brice <brice.c.aa@m.titech.ac.jp>
2025-06-24 17:42:04 +0900

    clarification and adding missing thread cleanup signals

--- src/ui/main_window.cpp
+++ src/ui/main_window.cpp
@@ -291,10 +291,10 @@ void MainWindow::ConfigureUi() {
  * @brief Sets up signal and slot connections to child threads and spins them up.
  *
  * @note In Qt, thread management for subclassed QThreads generally has 4 steps:
- *   1) Initialize a new QThread object
- *   2) Register a QThread signal to return data to a MainWindow handler
- *   3) Register the QThread's exit signal to its own destruction slot
- *   4) Spin off the QThread
+ *   1) Initialize a new QThread object as a child of MainWindow
+ *   2) Register its signals and slots to send/receive data to/from MainWindow
+ *   3) Register its exit signal to its own destruction slot
+ *   4) Spin it off
  * If the thread loops continuously, there is a fifth step:
  *   5) In the MainWindow destructor, interrupt or forcibly stop the QThread
  *
@@ -312,6 +312,10 @@ void MainWindow::InitializeThreads() {
 
     connect(ui_->pb_logshot, &QPushButton::clicked,  // log snapshot
             log_thread_, &LogThread::TakeLogShot);
+
+    // Thread cleanup
+    connect(log_thread_, &LogThread::finished,      // when thread exits
+            log_thread_, &LogThread::deleteLater);  // ... deallocate
 
     log_thread_->start();
     */
@@ -360,6 +364,10 @@ void MainWindow::InitializeThreads() {
             this, &MainWindow::HandleManipPosition);
     connect(arduino_thread_, &ArduinoThread::ReportWaterStatus,  // receive info
             this, &MainWindow::HandleWaterStatus);
+
+    // Thread cleanup
+    connect(arduino_thread_, &ArduinoThread::finished,      // when thread exits
+            arduino_thread_, &ArduinoThread::deleteLater);  // ... deallocate
 
     arduino_thread_->start();
 
