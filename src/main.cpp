fd4ef645b7533737fb1dac5e182fac5ac728440d
christian-brice <brice.c.67b9@m.isct.ac.jp>
2025-07-14 20:11:26 +0900

    control computer uses dark theme

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char* argv[]) {
 
     // Initialize the app
     QApplication app(argc, argv);
-    QIcon::setThemeName("Papirus");
+    QIcon::setThemeName("Papirus-Dark");  // "Papirus" for light theme
 
     // Display the main app window
     MainWindow w_main;
