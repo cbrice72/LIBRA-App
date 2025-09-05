5a49a7f1895351f11389f45525c441e32d1e5ac0
christian-brice <brice.c.67b9@m.isct.ac.jp>
2025-09-05 16:12:51 +0900

    changed bug where hebi published to ros ecosystem in deg instead of rad

--- src/thread/hebi_thread.cpp
+++ src/thread/hebi_thread.cpp
@@ -485,8 +485,10 @@ void HebiThread::PublishState() {
 
     // Populate the message and publish it
     for (uint8_t i = 0; i < n_actuators_; ++i) {
-        state_msg_.position[i] = actual_pos_map.at(static_cast<Joint::Name>(i));
-        state_msg_.velocity[i] = actual_vel_map.at(static_cast<Joint::Name>(i));
+        state_msg_.position[i] = actual_pos_map.at(static_cast<Joint::Name>(i))
+                                 * kDegToRad;
+        state_msg_.velocity[i] = actual_vel_map.at(static_cast<Joint::Name>(i))
+                                 * kDegToRad;
         state_msg_.effort[i] = actual_eff_map.at(static_cast<Joint::Name>(i));
     }
 
